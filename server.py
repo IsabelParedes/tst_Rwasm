e2a0abda02bc6f7163240014bc264369e48235f2
Isabel Paredes <isabel.paredes@quantstack.net>
2024-08-22 14:47:25 +0200

    Populate

---
+++ server.py
@@ -0,0 +1,15 @@
+import http.server
+import socketserver
+
+class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        # Add the required headers for cross-origin isolation
+        self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
+        self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+        super().end_headers()
+
+PORT = 8001
+
+with socketserver.TCPServer(("", PORT), CustomHTTPRequestHandler) as httpd:
+    print(f"Serving at port {PORT}")
+    httpd.serve_forever()
